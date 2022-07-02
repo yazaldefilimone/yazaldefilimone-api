964dcdadb52b3a3261049835875a8e2868fc9d73
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-02 23:24:39 +0200

    feat: create repo of projects

---
+++ src/data/protocols/repositories/Iproject-repository.ts
@@ -0,0 +1,15 @@
+import { Project, ProjectStore } from '@/domain/entities';
+
+export interface IProjectRepository {
+  add: (data: IProjectRepository.Input) => Promise<{ id: string }>;
+  findByName: (data: { name: string }) => IProjectRepository.OutputAll;
+  findAll: () => Promise<ProjectStore[]>;
+  findByRepo: (data: { repo: string }) => IProjectRepository.Output;
+  delete: (data: { id: string }) => Promise<void>;
+}
+
+export namespace IProjectRepository {
+  export type Input = Project;
+  export type Output = Promise<ProjectStore | null>;
+  export type OutputAll = Promise<ProjectStore[] | null>;
+}
