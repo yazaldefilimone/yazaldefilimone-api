880131e3c95d4d5e55adcce68f5a245edca44bbf
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-03 01:44:29 +0200

    feat: create data of hanle controllers

--- src/main/adapters/express-adapter.ts
+++ src/main/adapters/express-adapter.ts
@@ -3,7 +3,11 @@ import { Request, Response } from 'express';
 
 export const expressAdapter = (controller: IController) => {
   return async (request: Request, response: Response) => {
-    const httpResponse = await controller.handle();
+    const data = {
+      body: request.body,
+      params: request.params,
+    };
+    const httpResponse = await controller.handle(data);
 
     response.status(httpResponse.statusCode).json(httpResponse.body);
   };
