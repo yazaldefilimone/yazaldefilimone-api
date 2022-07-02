fc89e87744742af0c0736c55e6fec094e497a6dc
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-02 22:54:58 +0200

    feat: create interface of create project usecase

---
+++ src/domain/use-cases/ICreate-project-usecase.ts
@@ -0,0 +1,18 @@
+import { Either } from '@/shared/error-handler/either';
+import { AlreadyExistsError, InvalidParamError } from '@/domain/errors';
+
+export interface ICreateProjectUseCase {
+  perform: (data: ICreateProjectUseCase.Input) => Promise<ICreateProjectUseCase.Output>;
+}
+export namespace ICreateProjectUseCase {
+  export type Input = {
+    techs: string[];
+    title: string;
+    description: string;
+    banner: string;
+    url: string;
+    repo: string;
+  };
+
+  export type Output = Promise<Either<InvalidParamError | AlreadyExistsError, { id: string }>>;
+}
---
+++ src/domain/use-cases/index.ts
@@ -0,0 +1,1 @@
+export * from "./ICreate-project-usecase";
