d5a81a0f744fced988d80ab91aa1ba2d373c5976
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-02 23:24:54 +0200

    feat: create poststore

--- src/domain/entities/Post.ts
+++ src/domain/entities/Post.ts
@@ -1,8 +1,14 @@
-export type Post = {
+export type PostStore = {
   id?: string;
   tech: string;
   title: string;
   content: string;
   banner: string;
   createdAt?: string;
+};
+export type Post = {
+  tech: string;
+  title: string;
+  content: string;
+  banner: string;
 };
