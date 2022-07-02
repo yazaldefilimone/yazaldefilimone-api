513098d7b0bcc868478bf7b30f2d7640d1c8547b
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-03 01:45:26 +0200

    feat: add more insta of create project factory

--- src/main/factories/create-project-factory.ts
+++ src/main/factories/create-project-factory.ts
@@ -1,9 +1,11 @@
 import { CreateProjectUseCase } from '@/data/use-cases';
 import { ICreateProjectUseCase } from '@/domain/use-cases';
 import { ProjectRepository } from '@/infra/prisma/repositories';
+import { CreateProjectController } from '@/presentation/controllers';
 
-export const makeCreateProjectFactory = function (): ICreateProjectUseCase {
+export const makeCreateProjectFactory = function (): CreateProjectController {
   const projectRepository = new ProjectRepository();
   const createProjectUsecase = new CreateProjectUseCase(projectRepository);
-  return createProjectUsecase;
+  const createProjectController = new CreateProjectController(createProjectUsecase);
+  return createProjectController;
 };
