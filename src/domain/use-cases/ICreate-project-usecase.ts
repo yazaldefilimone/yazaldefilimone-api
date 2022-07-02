80d28fb96ae6495daa7671d0d324639c2a00ff21
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-02 23:25:26 +0200

    feat: use project entity

--- src/domain/use-cases/ICreate-project-usecase.ts
+++ src/domain/use-cases/ICreate-project-usecase.ts
@@ -1,18 +1,11 @@
 import { Either } from '@/shared/error-handler/either';
 import { AlreadyExistsError, InvalidParamError } from '@/domain/errors';
+import { Project } from '@/domain/entities';
 
 export interface ICreateProjectUseCase {
   perform: (data: ICreateProjectUseCase.Input) => ICreateProjectUseCase.Output;
 }
 export namespace ICreateProjectUseCase {
-  export type Input = {
-    techs: string[];
-    title: string;
-    description: string;
-    banner: string;
-    url: string;
-    repo: string;
-  };
-
+  export type Input = Project;
   export type Output = Promise<Either<InvalidParamError | AlreadyExistsError, { id: string }>>;
 }
