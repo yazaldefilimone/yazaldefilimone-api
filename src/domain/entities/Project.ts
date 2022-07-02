dbac6ed0b08814b46bcda9d9cfdd86404e1cb812
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-02 22:55:33 +0200

    feat: create project

--- src/domain/entities/Project.ts
+++ src/domain/entities/Project.ts
@@ -1,1 +1,10 @@
-export type Project = {};
+export type Project = {
+  id?: string;
+  techs: string[];
+  title: string;
+  description: string;
+  banner: string;
+  url: string;
+  repo: string;
+  createdAt?: string;
+};
