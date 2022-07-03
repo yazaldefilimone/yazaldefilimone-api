53e7be2342b24772fef3fead8f169a966da8ef0e
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-03 13:29:19 +0200

    fix: delete old migration

--- prisma/migrations/20220703104840_create_migration/migration.sql
+++ prisma/migrations/20220703104840_create_migration/migration.sql
@@ -30,7 +30,7 @@ CREATE TABLE "post" (
 -- CreateTable
 CREATE TABLE "stack" (
     "id" TEXT NOT NULL,
-    "name" TEXT[],
+    "name" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "projectId" TEXT,
 
@@ -41,10 +41,19 @@ CREATE TABLE "stack" (
 CREATE UNIQUE INDEX "project_id_key" ON "project"("id");
 
 -- CreateIndex
+CREATE UNIQUE INDEX "project_title_key" ON "project"("title");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "project_repo_key" ON "project"("repo");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "post_id_key" ON "post"("id");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "stack_id_key" ON "stack"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "stack_name_key" ON "stack"("name");
 
 -- AddForeignKey
 ALTER TABLE "stack" ADD CONSTRAINT "stack_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "project"("id") ON DELETE SET NULL ON UPDATE CASCADE;
