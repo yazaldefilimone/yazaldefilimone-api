5ee57aa37d144d5fc5f4a9dff842f78f8397e2a4
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-03 13:31:46 +0200

    refactor: add dafault format code

--- src/presentation/controllers/Find-project-controller.ts
+++ src/presentation/controllers/Find-project-controller.ts
@@ -3,19 +3,24 @@ import { IController, HttpResponse, ok, serverError } from '@/presentation/proto
 import { serializePagination } from '@/shared/utils';
 
 export class FindProjectController implements IController {
-  constructor(private readonly IFindProjectUseCase: IFindProjectUseCase) {}
-  async handle(request: any): Promise<HttpResponse> {
+  private readonly findProjectUseCase: IFindProjectUseCase;
+  constructor(findProjectUseCase: IFindProjectUseCase) {
+    this.findProjectUseCase = findProjectUseCase;
+  }
+
+  async handle(request: { body: any; params: any; query: any }): Promise<HttpResponse> {
     try {
       const { page, limit } = request.query;
       const paginationProps = serializePagination({
         page: Number(page) || 1,
-        limit: Number(limit) || 10,
+        limit: Number(limit) || 4,
       });
 
-      const result = await this.IFindProjectUseCase.perform(paginationProps);
+      const result = await this.findProjectUseCase.perform(paginationProps);
 
       return ok(result);
     } catch (error) {
+      console.log(error);
       return serverError(error);
     }
   }
