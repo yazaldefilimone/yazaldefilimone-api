6361d373a645482c6292bf448b6d3b625f3f1a10
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-03 13:34:11 +0200

    fix: add  other class in export

--- src/data/use-cases/index.ts
+++ src/data/use-cases/index.ts
@@ -1,3 +1,4 @@
 export * from './Create-project-usecase';
 export * from './Find-project-usecase';
 export * from './Find-title-project-usecase';
+export * from './Find-tech-project-usecase';
--- src/presentation/controllers/index.ts
+++ src/presentation/controllers/index.ts
@@ -1,1 +1,4 @@
 export * from './Create-project-controller';
+export * from './Find-project-controller';
+export * from './Find-title-project-controller';
+export * from './Find-tech-project-controller';
